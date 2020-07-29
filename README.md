e4e1ea89c97f2019e1b601137a7b06865b78356d
Eric Verner <everner@gsu.edu>
2020-07-29 17:33:03 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# AWS Command Line MFA Set up                                                                                                                
+These scripts are wrappers for the AWS command-line interface (CLI) multifactor authentication (MFA) setup procedure, which can be found [here](https://aws.amazon.com/premiumsupport/knowledge-center/authenticate-mfa-cli/). Using the AWS CLI with an MFA device is a common use case, but the procedure requires a lot of manual information retrieval, copying, and pasting. These script are meant to ease the process. 
+
+## Usage
+
+1. Set up `.env` file
+  - Copy `.env.example` to `.env`.
+  - Fill in your MFA ARN code for the `ARN` variable. This can be found on your security credentials page on AWS ([here](https://console.aws.amazon.com/iam/home?#/security_credentials)). The ARN code is the Assigned MFA device under the Multi-factor authentication (MFA) section.
+    - e.g., `arn:aws:iam::111122223333:mfa/username`
+  - If you like, you can change the filename where the environment variables will be stored as well. This is stored as the `ENV_FILE` variable.
+  - Save the file.
+
+2. Run the script
+  - Open your MFA device and find your 6-digit code (e.g., 123456)
+  - Run the script in one of the following ways:
+
+    `bash setup_aws.sh -t 123456`
+
+    `bash setup_aws.sh --token 123456`
+
+    `bash setup_aws.sh -v -t 123456`
+
+    `bash setup_aws.sh --verbose --token 123456`
+
+  - This will request your temporary credentials from AWS, which must be set as environment variables before you can use the CLI.
+
+3. Set environment variables
+  - Set the environment variables fetched by the script
+
+    `source envs.sh`
+    
+    (or whatever your `ENV_FILE` is) 
+
+4. Run your AWS CLI commands
+  - e.g., `aws s3 ls` 
+
+5. Unset your environment variables or close your shell
+  `source unset.sh`
+
+## Dependencies
+
+1. You must have an AWS MFA device set up. 
+2. You must have an AWS CLI access key set up. This can be done on your AWS security credentials page.
+3. Install [jq](https://stedolan.github.io/jq/download/), a command-line utility that handles JSON.
---
+++ setup_aws.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# This script requests AWS CLI MFA credentials and saves them as 
+# environment variables to a local file.
+
+function load_env {
+    set -a
+    [ -f .env ] && . .env
+    set +a
+}
+
+usage() { echo "Usage: $0 -t mfa_code" 1>&2; exit 1; }
+
+VERBOSE=false
+
+# Parse command line arguments 
+
+#while getopts ":t:v:" opt; do
+#  case $opt in
+#    t) TOKEN="$OPTARG"
+#      ;;
+#    v) VERBOSE=true
+#      ;;
+#    *) usage
+#      ;;
+#  esac
+#done
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -t|--token) TOKEN="$2"; shift ;;
+    -v|--verbose) VERBOSE=true ;;
+    *) echo "Unknown parameter passed: $1"; usage ;;
+  esac
+  shift
+done
+
+if [ -z "${TOKEN+x}" ]
+then
+  echo "MFA token not set"
+  usage
+  exit 1
+fi
+
+load_env
+
+if $VERBOSE
+then
+  echo
+  echo "Local variables"
+  echo "---------------"
+  echo "ARN: ${ARN}"
+  echo "TOKEN: ${TOKEN}"
+  echo "ENV_FILE: ${ENV_FILE}"
+  echo
+fi
+
+# Request credentials from AWS
+aws_command="aws sts get-session-token --serial-number ${ARN} --token-code ${TOKEN}" 
+
+command="${aws_command}"
+
+if $VERBOSE
+then
+  printf "Command: \n %s\n" "$command"
+fi
+
+echo
+echo "Requesting credentials..."
+creds=`eval " $command"`
+
+if [ -z "${creds}" ]
+then
+  echo "ERROR: Request failed"
+  exit 0
+fi
+
+echo "Credentials received!"
+
+if $VERBOSE
+then
+  printf "Credentials: \n %s\n" "$creds"
+fi
+
+# Parse response
+aws_access_key=`echo $creds | jq '.Credentials.AccessKeyId' | sed 's/"//g'`
+aws_secret_access_key=`echo $creds | jq '.Credentials.SecretAccessKey' | sed 's/"//g'`
+aws_session_token=`echo $creds | jq '.Credentials.SessionToken' | sed 's/"//g'`
+expiration=`echo $creds | jq '.Credentials.Expiration' | sed 's/"//g'`
+
+# Save credentials to environment variables in separate file
+if $VERBOSE
+then
+  echo
+  echo "Environment variables will be saved to ${ENV_FILE} as follows:"
+  echo "AWS_ACCESS_KEY_ID: ${aws_access_key}"
+  echo "AWS_SECRET_ACCESS_KEY: ${aws_secret_access_key}"
+  echo "AWS_SESSION_TOKEN: ${aws_session_token}"
+fi
+
+echo "export AWS_ACCESS_KEY_ID=$aws_access_key" > $ENV_FILE
+echo "export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key" >> $ENV_FILE
+echo "export AWS_SESSION_TOKEN=$aws_session_token" >> $ENV_FILE
+
+echo
+echo "Finished!"
+echo
+echo "Your credentials will expire on ${expiration}"
+echo
+echo "Type 'source ${ENV_FILE}' to export environment variables"
+echo
+echo "After you are done using the AWS CLI, close your terminal"
+echo "or unset your environment variables by typing 'source unset.sh'."
+echo
